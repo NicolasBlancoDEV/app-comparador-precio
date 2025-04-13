500e13e16b9b7b28cf90b267c92d7a5adf883c65
Nicolas <nicolasblancodev@gmail.com>
2025-04-13 17:42:06 -0300

    Añadir gunicorn y configurar startCommand para Render

--- app.py
+++ app.py
@@ -651,6 +651,4 @@ def clear_cart():
 with app.app_context():
     init_db()
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+# No necesitamos app.run() porque Render usará gunicorn
